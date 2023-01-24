58b0be1efaaa8184dd23f6a7c8277574e07ac2b6
olawalekareemdev <olawalekareemdev@gmail.com>
2023-01-24 03:13:06 +0100

    feat:event driving consumer setup

--- consumer.py
+++ consumer.py
@@ -1,5 +1,7 @@
 import pika
+import json
 
+from main import Product, db
 
 params = pika.URLParameters('amqps://hsfvjmxb:NnXl1xRXc4nx5YPeGhalfOkCm6S6O0Hx@possum.lmq.cloudamqp.com/hsfvjmxb')
 connection = pika.BlockingConnection(params)
@@ -11,8 +13,30 @@ channel.queue_declare(queue='main')
 # declare what to do with the meossage
 def callback(ch, method, properties, body):
     print('Received in main')
-    print(body)
-    pass
+    data = json.loads(body)
+    print(data)
+
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('product created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('product updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('product deleted')
+  
+   
 
 
 # declare consume type and start consumming
